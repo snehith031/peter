e0460835f24646b387bfddd372e622ea71d53858
mrjonny <104708305+mrjonny@users.noreply.github.com>
2022-05-04 23:42:07 +0530

    Update flaskloginform.py

--- flask/flaskbasicindex/flaskloginform.py
+++ flask/flaskbasicindex/flaskloginform.py
@@ -1,38 +1,18 @@
 from flask import *
-
 app = Flask(__name__)
 
-
-@app.route('/error')
-def error():
-    return "<p><strong>Enter correct password</strong></p>"
-
-
 @app.route('/')
-def login():
-    return render_template("login.html")
+def hello():
+    return "Hello This is my first Webpage created using Flask Framework"
+
+@app.route('/index')
+def ggmg():
+    return render_template("index.html")
+
+@app.route('/about')
+def goo():
+    return render_template("about.html")
 
 
-@app.route('/success', methods=['POST'])
-def success():
-    if request.method == "POST":
-        email = request.form['email']
-        password = request.form['pass']
-
-    if password == "jtp":
-        resp = make_response(render_template('success.html'))
-        resp.set_cookie('email', email)
-        return resp
-    else:
-        return redirect(url_for('error'))
-
-
-@app.route('/viewprofile')
-def profile():
-    email = request.cookies.get('email')
-    resp = make_response(render_template('profile.html', name=email))
-    return resp
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
